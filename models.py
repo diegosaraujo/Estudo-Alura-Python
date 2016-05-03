2f42ee0448e9aadac3c10ce3e85da300c3ff9a60
diegosaraujo <diesaraujo@gmail.com>
2016-05-03 15:16:04 -0300

    Changed method name 'Imprimir Dados do Perfil'

--- models.py
+++ models.py
@@ -8,5 +8,5 @@ class Perfil():
 		self.telefone = telefone
 		self.empresa = empresa
 
-	def imprimir(self):
+	def imprimir_dados_perfil(self):
 		print (('Nome: %s, Telefone: %s, Empresa: %s')%(self.nome, self.telefone, self.empresa))
