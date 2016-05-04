4a078e6e5c604337ef7556c7685174742d0741f9
diegosaraujo <diesaraujo@gmail.com>
2016-05-03 23:35:53 -0300

    Created more one class for the chalenge

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ class Perfil():
 		print (('Nome: %s, Telefone: %s, Empresa: %s')%(self.nome, self.telefone, self.empresa))
 
 class Data(object):
-   'Classe para formatar datas'
+   'Classe para formatar datas - Desafio Exercicio'
 
    def __init__(self, dia, mes, ano):
       self.dia = dia
@@ -23,3 +23,15 @@ class Data(object):
       print (('%s/%s/%s') % (self.dia, self.mes, self.ano))
 
 
+class Pessoa(object):
+   'Classe para Pessoa que calcula o IMC - Desafio Exercicio'
+
+   def __init__(self, nome, peso, altura):
+      self.nome = nome
+      self.peso = float(peso)
+      self.altura = float(altura)
+
+
+   def imprime_imc(self):
+      imc = self.peso / (self.altura **2)
+      print (('IMC de %s: %s') % (self.nome, imc))
