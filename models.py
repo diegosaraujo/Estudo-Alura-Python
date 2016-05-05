c0a6e3f2b177842ac4514a0acf8e020f94726416
diegosaraujo <diesaraujo@gmail.com>
2016-05-05 10:47:23 -0300

    OverWrite on Python

--- models.py
+++ models.py
@@ -59,3 +59,21 @@ class Pessoa(object):
    def imprime_imc(self):
       imc = self.peso / (self.altura **2)
       print (('IMC de %s: %s') % (self.nome, imc))
+
+
+#Sobrescrita de Método, neste caso o do calcular_imposto
+class Conta(object):
+    def __init__(self, titular, saldo):
+        self.titular = titular
+        self.saldo = saldo
+
+    def calcular_imposto(self):
+        self.saldo = self.saldo * 0.10
+        return self.saldo
+
+
+class ContaCorrente(Conta):
+    def __init__(self, titular, saldo):
+        super(ContaCorrente, self).__init__(titular, saldo)
+    def calcular_imposto(self):
+        return super(ContaCorrente, self).calcular_imposto() + 20
